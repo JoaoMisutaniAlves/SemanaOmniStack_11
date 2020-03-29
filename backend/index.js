7a15b2432fab657232db433651b97b02677377c2
João Misutani <jp_ma_8@hotmail.com>
2020-03-29 13:47:16 -0300

    Json responde

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express();
 
 app.get('/', (request,response) =>{
-return response.send('Hello Word!');
+return response.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'João Pedro Misutani Alves'
+});
 });
 
 app.listen(3333);
